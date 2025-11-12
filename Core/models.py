cfe16c2505bad0ba6bf1c2be4792a7b93d5b20b2
Gopikadas42 <gopikadazz@gmail.com>
2025-11-12 13:51:55 +0530

    first phase

---
+++ Core/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+        ('seller', 'Seller'),
+    )
+
+    contact = models.CharField(max_length=20)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.username} ({self.role})"
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+
+    def _str_(self):
+        return self.category_name
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sub_category_name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def _str_(self):
+        return f"{self.sub_category_name} ({self.category.category_name})"
+
---
+++ Seller/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from Core.models import User, SubCategory
+
+
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="seller_profile")
+    profile_image = models.ImageField(upload_to='seller_profiles/', null=True, blank=True)
+    shop_name = models.CharField(max_length=150)
+    address = models.TextField()
+    website = models.URLField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.shop_name
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name="products")
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name="products")
+    product_name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    slug=models.SlugField()
+    rating = models.FloatField(default=0)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.product_name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    product_img = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return f"Image for {self.product.product_name}"
+
+
