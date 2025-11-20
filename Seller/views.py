a393759495c3300309dfa9793c6afa7595936e88
praseetha ku <praseeachu7239@gmail.com>
2025-11-20 16:25:52 +0530

    second commit

--- Seller/views.py
+++ Seller/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render,redirect
+from django.utils.text import slugify
+
 from Core.models import User, SubCategory
-from User.models import Order
+from User.models import Order, OrderItem, Review
+from decorators.decorators import role_required
 from .models import Seller, Product, ProductImage
 from django.contrib.auth import authenticate,login,logout
 
@@ -30,8 +33,19 @@ def user_register(request):
     return render(request,'seller/seller_register.html')
 
 @login_required(login_url='seller/login')
+@role_required("seller", login_url="/seller/login")
 def seller_dashboard(request):
-    return render(request, 'seller/seller_dashboard.html')
+    seller = Seller.objects.get(user=request.user)
+    products = Product.objects.filter(seller=seller)
+    total_products = products.count()
+    order =OrderItem.objects.filter(product__seller=seller)
+    total_order=order.count()
+    productsvalues = Product.objects.filter(seller=seller).order_by('-id')
+
+    return render(request, 'seller/seller_dashboard.html',{
+        'products': products,
+        'total_products': total_products,'total_order':total_order,'productsvalues':productsvalues})
+
 def user_login(request):
     if request.method=="POST":
         name=request.POST.get('username')
@@ -45,6 +59,7 @@ def user_login(request):
 
     return render(request,'seller/seller_login.html')
 @login_required(login_url='/seller/login')
+@role_required("seller", login_url="/seller/login")
 def Create_Product(request):
     subcategories = SubCategory.objects.all()
     print(subcategories)
@@ -64,12 +79,13 @@ def Create_Product(request):
         product.product_name = request.POST.get("product_name")
         product.price = request.POST.get("price")
         product.description = request.POST.get("description")
-        product.slug = request.POST.get("slug")
+        product.slug = slugify(product.product_name)
         product.rating = request.POST.get("rating")
         product.stock = request.POST.get("stock")
         product.save()
 
         image = request.FILES.get("image")
+        print("imagefound")
         if image:
             ProductImage.objects.create(product=product, product_img=image)
 
@@ -80,6 +96,7 @@ def Create_Product(request):
         'products': products,
     })
 @login_required(login_url='/seller/login')
+@role_required("seller", login_url="/seller/login")
 def product_delete(request, slug):
     try:
         product = Product.objects.get(slug=slug, seller__user=request.user)
@@ -91,6 +108,7 @@ def product_delete(request, slug):
 
 
 @login_required(login_url='/seller/login')
+@role_required("seller", login_url="/seller/login")
 def edit_product(request, slug):
     product = Product.objects.get(slug=slug)
     subcategories = SubCategory.objects.all()
@@ -116,13 +134,33 @@ def edit_product(request, slug):
         'subcategories': subcategories,
     })
 @login_required(login_url='/seller/login')
+@role_required("seller", login_url="/seller/login")
 def order_list(request):
     seller = Seller.objects.get(user=request.user)
-    order=Order.objects.filter(order_items__product__seller=seller)
-    paginator = Paginator(order, 10)
+
+
+    order_items = OrderItem.objects.filter(product__seller=seller).select_related('order', 'product')
+
+    paginator = Paginator(order_items, 10)
     page_number = request.GET.get('page')
     orders = paginator.get_page(page_number)
+
     return render(request, 'seller/seller_orders.html', {
         'orders': orders
     })
+def order_single_list(request, id):
+    seller = Seller.objects.get(user=request.user)
+    order = OrderItem.objects.get(id=id, product__seller=seller)
+    print(order)
+    print(id)
+    return render(request, 'seller/seller_order_single.html', {'order': order})
+def product_single(request,slug):
+    product=Product.objects.get(slug=slug)
+    review=Review.objects.filter(product=product)
+    print(review)
+    return render(request,'seller/product_details.html',{'product':product,'review':review})
 
+def seller_profile(request):
+    seller=Seller.objects.get(user=request.user)
+    print(seller)
+    return render(request,'seller/seller_profile.html',{'seller':seller})
--- User/admin.py
+++ User/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from User.models import Order, OrderItem
+from User.models import Order, OrderItem, Customer, Review
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Customer)
+admin.site.register(Review)
