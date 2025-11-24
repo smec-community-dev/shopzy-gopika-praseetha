2fc2dc435f82e5e7e80794f4f7579d5d66fd4b1f
praseetha ku <praseeachu7239@gmail.com>
2025-11-24 15:16:05 +0530

    conflict solve1

--- User/admin.py
+++ User/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-from .models import Customer,Cart,Order,OrderItem,Address,Review,ReviewImage
 
-from User.models import Order, OrderItem, Customer, Review, ReviewImage
 
-# Register your models here.
-<<<<<<< HEAD
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Customer)
-admin.site.register(Review)
-admin.site.register(ReviewImage)
-=======
+from User.models import Order, OrderItem, Customer, Review, ReviewImage, Cart, Address
 
 admin.site.register(Customer)
 admin.site.register(Cart)
@@ -19,4 +10,4 @@ admin.site.register(OrderItem)
 admin.site.register(Address)
 admin.site.register(Review)
 admin.register(ReviewImage)
->>>>>>> 975012024e8fa9c0ef5abb883b4907e10db4c7fb
+
